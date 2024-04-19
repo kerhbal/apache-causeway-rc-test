f05667fbd95d8dc77ba4a901e41d0c3b2f309e2a
Ilmar Marques <ilmar.marques@localcoinatm.com>
2024-04-19 07:19:21 -0400

    - Added datadog integration built-in with logs & metrics

--- webapp/src/main/java/domainapp/webapp/application/ApplicationModule.java
+++ webapp/src/main/java/domainapp/webapp/application/ApplicationModule.java
@@ -1,5 +1,9 @@
 package domainapp.webapp.application;
 
+import io.micrometer.core.instrument.MeterRegistry;
+
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -10,5 +14,10 @@ import domainapp.modules.simple.SimpleModule;
 @Import(SimpleModule.class)
 @ComponentScan
 public class ApplicationModule {
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> commonTags() {
+        return r -> r.config().commonTags("host", "myapp-dev");
+    }
 
 }
